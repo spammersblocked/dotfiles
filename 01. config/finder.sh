71b127fa1918b71924748c26420eae2c2c850dab
asish kumar <asishkumar@***.com>
2025-04-20 03:00:07 +0530

    finder - column views

--- 01. config/finder.sh
+++ 01. config/finder.sh
@@ -11,4 +11,10 @@ defaults write com.apple.finder ShowPathbar -bool true
 # show status bar
 defaults write com.apple.finder ShowStatusBar -bool true
 
+# show folder in column view
+defaults write com.apple.Finder FXPreferredViewStyle clmv
+
+# delete all .DS_Store
+sudo find ~ -name ".DS_Store" -delete
+
 killall Finder;
